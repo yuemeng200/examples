1d30d4b85a89f7442110245e9de3422d2be17745
shichangzhen <92299558+yuemeng200@users.noreply.github.com>
2024-10-27 22:34:56 +0800

    temp

--- packages/vercel-api/api/hello.js
+++ packages/vercel-api/api/hello.js
@@ -1,8 +1,6 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 export default async function handler(req, res) {
-  // const response = awaitgit  axios.get('https://jsonplaceholder.typicode.com/todos/1');
-  res.status(200).json({
-    message: 'Hello, world!',
-  });
+  const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+  res.status(200).json(response.data);
 }
