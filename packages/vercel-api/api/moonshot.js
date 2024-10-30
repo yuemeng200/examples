99e938938033e0984b48ecf1ab42220b41e14935
shichangzhen <92299558+yuemeng200@users.noreply.github.com>
2024-10-30 13:15:45 +0800

    temp

---
+++ packages/vercel-api/api/moonshot.js
@@ -0,0 +1,22 @@
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.MOONSHOT_API_KEY,
+  baseURL: "https://api.moonshot.cn/v1",
+});
+
+export default async function handler(req, res) {
+  const { prompt, message } = req.body;
+
+  const response = await openai.chat.completions.create({
+    model: "moonshot-v1-8k",
+    messages: [{
+      role: "system", content: "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。",
+      role: "user", content: message
+    }],
+    temperature: 0.3
+  });
+  res.status(200).json({
+    message: response.choices[0].message.content
+  });
+}
