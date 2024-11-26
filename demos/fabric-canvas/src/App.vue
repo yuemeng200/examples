965d1cb4908c376cdabeb81fe3d6da7c8592e8b4
shichangzhen <92299558+yuemeng200@users.noreply.github.com>
2024-11-26 12:10:04 +0800

    temp

---
+++ demos/fabric-canvas/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <CustomCanvas />
+</template>
+
+<script lang="ts" setup>
+import CustomCanvas from './components/CustomCanvas.vue'
+</script>
+
+<style lang="less">
+#app {
+}
+</style>
